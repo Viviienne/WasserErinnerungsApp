0019a84bffe6bdbf922284e3b9493cbf1fdacabc
Viviienne <120227203+Viviienne@users.noreply.github.com>
2023-04-27 10:40:27 +0200

    fix rest

--- test.js
+++ test.js
@@ -16,31 +16,21 @@ glass1.addEventListener("click", function() {
 });
 
 const glass2 = document.getElementById("glass2");
-function changeColor() {
-glass2.style.backgroundColor = "#3a226c";
-
-glass2.addEventListener("click", changeColor);
-}
-
+glass2.addEventListener("click", function() {
+  glass2.style.backgroundColor = "#3a226c";
+});
 
 const glass3 = document.getElementById("glass3");
-function changeColor() {
-glass3.style.backgroundColor = "#3a226c";
-
-glass3.addEventListener("click", changeColor);
-}
-
+glass3.addEventListener("click", function() {
+  glass3.style.backgroundColor = "#3a226c";
+});
 
 const glass4 = document.getElementById("glass4");
-function changeColor() {
-glass4.style.backgroundColor = "#3a226c";
-}
-glass4.addEventListener("click", changeColor);
-
-
+glass4.addEventListener("click", function() {
+  glass4.style.backgroundColor = "#3a226c";
+});
 
 const glass5 = document.getElementById("glass5");
-function changeColor() {
-glass5.style.backgroundColor = "#3a226c";
-}
-glass5.addEventListener("click", changeColor);
+glass5.addEventListener("click", function() {
+  glass5.style.backgroundColor = "#3a226c";
+});
