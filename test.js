70e653d624a7b56a3c01b49b883736b21fa16ac7
Viviienne <120227203+Viviienne@users.noreply.github.com>
2023-04-27 10:06:14 +0200

    click events hinzugefügt und typescript commentar

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
-const button = document.getElementById("remind-laterButton")
+const button = document.getElementById("remind-laterButton") // as HTMLButtonElement;
 
 button.addEventListener("click", () => {
     Notification.requestPermission().then(perm => {
-        if (perm === "granted" ){
-            new Notification ("Test")
+        if (perm === "granted"){
+            new Notification ("Erinnerung",{
+                body: "Erfrische Geist und Körper - es ist Zeit, Wasser zu trinken"
+            })
         }
     })
 })
+
+
+const glass1 = document.getElementById("glass1") //as HTMLDivElement;
+glass1.addEventListener ("click"), function (){
+glass1.style.backgroundColor = "blue";
+}
